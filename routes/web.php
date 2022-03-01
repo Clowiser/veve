f38a9f1eb819d20617c6a54ff67b4ffd49ebe2e8
alexnicolo73 <alex.nicolo@le-campus-numerique.fr>
2022-03-01 11:07:28 +0100

    add route /

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [HomeController::class, 'showHome']);
 Route::get('home', [HomeController::class, 'showHome']);
 Route::get('product', [ProductController::class, 'showListe']);
 Route::get('product/{id}',[ProductController::class, 'showProduit']);
