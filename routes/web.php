81510f17a0fc173143193ef368f1eddacde996d3
Seb <jssebastienjacquelin@hotmail.com>
2022-03-01 11:02:42 +0100

    navbar changes

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('Home', [HomeController::class, 'showHome']);
+Route::get('home', [HomeController::class, 'showHome']);
 Route::get('product', [ProductController::class, 'showListe']);
 Route::get('product/{id}',[ProductController::class, 'showProduit']);
 Route::get('cart', [CartController::class, 'showCart']);
