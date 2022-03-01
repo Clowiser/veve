058548fae2ca55902dcf56297f8425bbec45c740
Seb <jssebastienjacquelin@hotmail.com>
2022-03-01 10:17:15 +0100

    navbar change

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,18 +7,34 @@
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active text-white" aria-current="page" href="{{ url('/') }}">Accueil</a>
-                <a class="nav-link active text-white" href="{{ url('/product') }}">Boutique</a>
-                {{--                <a class="nav-link active text-dark" href="{{ url('/product/name') }}">Boutique par Nom</a>--}}
-                {{--                <a class="nav-link active text-dark" href="{{ url('/product/prix') }}">Boutique par Prix</a>--}}
-                <a class="nav-link active text-white" href="{{ url('/cart') }}">Panier</a>
-                {{--                <a class="nav-link active text-dark" href="{{ url('/backoffice') }}">Backoffice</a>--}}
-                {{--                <a class="nav-link active text-dark" href="{{ url('/backoffice/create') }}">Backoffice Create</a>--}}
-            </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown link
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
