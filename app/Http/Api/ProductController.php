ad78c54659f972e0ba6a17af12e64a0339a4d413
Seb <jssebastienjacquelin@hotmail.com>
2022-03-08 09:11:23 +0100

    product controller created for api

---
+++ app/Http/Api/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Api;
+
+class ProductController
+{
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+}
